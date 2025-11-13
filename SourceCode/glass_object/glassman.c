d55c8926760b4b49679dbf9d8837f8c52642e9be
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2025-11-13 02:03:46 -0500

    Add files via upload

--- SourceCode/glass_object/glassman.c
+++ SourceCode/glass_object/glassman.c
@@ -128,6 +128,17 @@ typedef struct
  GeglNode *string;
  GeglNode *hyperopacity;
  GeglNode *c2a;
+ GeglNode *inputx;
+ GeglNode *src;
+ GeglNode *opacityx;
+ GeglNode *colorx;
+ GeglNode *growx;
+ GeglNode *overx;
+ GeglNode *srcinx;
+ GeglNode *blurx;
+ GeglNode *translatex;
+
+ GeglNode *fix;
 }State;
 
 static void attach (GeglOperation *operation)
@@ -135,7 +146,7 @@ static void attach (GeglOperation *operation)
   GeglNode *gegl = operation->node;
   GeglProperties *o = GEGL_PROPERTIES (operation);
   GeglColor *grey = gegl_color_new ("#6a6a6a");
-
+  GeglColor *black_color = gegl_color_new ("rgb(0.0,0.0,0.0)");
   State *state = o->user_data = g_malloc0 (sizeof (State));
 
 /*new child node list is here, this is where starred nodes get defined
@@ -146,11 +157,16 @@ static void attach (GeglOperation *operation)
 
  state->smooth = gegl_node_new_child (gegl, "operation", "gegl:gaussian-blur", "clip-extent", FALSE, "abyss-policy", 0, NULL);
 
- state->dropshadow = gegl_node_new_child (gegl, "operation", "gegl:dropshadow", NULL);
+ state->dropshadow = gegl_node_new_child (gegl, "operation", "gegl:dropshadow", "opacity", 1.0, NULL);
 
  state->metallic = gegl_node_new_child (gegl, "operation", "lb:metallic", NULL);
 
  state->hyperopacity = gegl_node_new_child (gegl, "operation", "gegl:opacity", NULL);
+
+ state->fix = gegl_node_new_child (gegl, "operation", "gegl:median-blur", "radius", 0, "abyss-policy", 0, NULL);
+
+
+ state->opacityx = gegl_node_new_child (gegl, "operation", "gegl:opacity", NULL);
 
  state->c2a = gegl_node_new_child (gegl, "operation", "gegl:color-to-alpha", "color", grey, "transparency-threshold", 0.079, "opacity-threshold", 1.0,  NULL);
 
@@ -158,13 +174,28 @@ static void attach (GeglOperation *operation)
 " id=1 over aux=[ ref=1 ]  gaussian-blur std-dev-x=0.2 std-dev-y=0.2 clip-extent=false motion-blur-linear length=0.5 "\
 
  state->string = gegl_node_new_child (gegl, "operation", "gegl:gegl", "string", glassstring, NULL);
-/*meta redirect property to new child orders go here
- * 
 
 
+   state->src      = gegl_node_new_child (gegl, "operation", "gegl:src", NULL);
 
- gegl_operation_meta_redirect (operation, "propertyname", state->newchildname,  "originalpropertyname");
-*/
+   state->inputx      = gegl_node_new_child (gegl, "operation", "gegl:nop", NULL);
+
+   state->overx      = gegl_node_new_child (gegl, "operation", "gegl:over", NULL);
+   state->translatex = gegl_node_new_child (gegl, "operation", "gegl:translate", NULL);
+
+   state->blurx      = gegl_node_new_child (gegl, "operation", "gegl:gaussian-blur",
+                                         "clip-extent", FALSE,
+                                         "abyss-policy", 0,
+                                         NULL);
+   state->growx      = gegl_node_new_child (gegl, "operation", "gegl:median-blur",
+                                         "percentile",       100.0,
+                                         "alpha-percentile", 100.0,
+                                         "abyss-policy",     GEGL_ABYSS_NONE,
+                                         NULL);
+   state->srcinx    = gegl_node_new_child (gegl, "operation", "gegl:src-in", NULL);
+   state->colorx     = gegl_node_new_child (gegl, "operation", "gegl:color",
+                                   "value", black_color,
+                                   NULL);
 
 }
 
@@ -175,20 +206,27 @@ update_graph (GeglOperation *operation)
   State *state = o->user_data;
   if (!state) return;
 
-  gegl_node_link_many (state->input, state->smooth, state->metallic, state->dropshadow, state->c2a, state->string, state->hyperopacity,  state->output,  NULL);
+  gegl_node_link_many (state->input, state->smooth, state->metallic, state->inputx, state->src,   state->c2a, state->string, state->hyperopacity, state->fix, state->output,  NULL);
+
+  gegl_node_connect (state->src, "aux", state->overx, "output");
+  gegl_node_link_many (state->inputx, state->growx, state->srcinx, state->blurx, state->opacityx, state->translatex, state->overx,
+                       NULL);
+  gegl_node_connect (state->overx, "aux", state->inputx, "output");
+  gegl_node_connect (state->srcinx, "aux", state->colorx, "output");
 
   gegl_operation_meta_redirect (operation, "smooth", state->smooth, "std-dev-y");
   gegl_operation_meta_redirect (operation, "smooth", state->smooth, "std-dev-x");
   gegl_operation_meta_redirect (operation, "glassrotate1", state->metallic, "solar1");
   gegl_operation_meta_redirect (operation, "glassrotate2", state->metallic, "solar2");
   gegl_operation_meta_redirect (operation, "glassrotate3", state->metallic, "solar3");
-  gegl_operation_meta_redirect (operation, "color", state->dropshadow, "color");
-  gegl_operation_meta_redirect (operation, "grow-radius", state->dropshadow, "grow-radius");
-  gegl_operation_meta_redirect (operation, "radius", state->dropshadow, "radius");
-  gegl_operation_meta_redirect (operation, "opacity", state->dropshadow, "opacity");
+  gegl_operation_meta_redirect (operation, "color", state->colorx, "value");
+  gegl_operation_meta_redirect (operation, "grow-radius", state->growx, "radius");
+  gegl_operation_meta_redirect (operation, "radius", state->blurx, "std-dev-x");
+  gegl_operation_meta_redirect (operation, "radius", state->blurx, "std-dev-y");
+  gegl_operation_meta_redirect (operation, "opacity", state->opacityx, "value");
   gegl_operation_meta_redirect (operation, "hyperopacity", state->hyperopacity, "value");
-  gegl_operation_meta_redirect (operation, "y", state->dropshadow, "y");
-  gegl_operation_meta_redirect (operation, "x", state->dropshadow, "x");
+  gegl_operation_meta_redirect (operation, "y", state->translatex, "y");
+  gegl_operation_meta_redirect (operation, "x", state->translatex, "x");
 
 }
 
@@ -206,7 +244,7 @@ GeglOperationMetaClass *operation_meta_class = GEGL_OPERATION_META_CLASS (klass)
     "name",        "lb:glassobject",
     "title",       _("Glass Object"),
     "reference-hash", "ivotedfornobodybecauseagorism",
-    "description", _("Turn subject/object into Glass with the addition of a shadow/outline because those effects do not apply normally on glass"),
+    "description", _("Turn subject/object into glass with the addition of a shadow casting"),
 /*<Image>/Colors <Image>/Filters are top level menus in GIMP*/
     "gimp:menu-path", "<Image>/Filters/Artistic",
     "gimp:menu-label", _("Glass transformation..."),
